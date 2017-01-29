191c315610614b98ba8c63d96f5a2aab02fac92b
SerafimovichEugene <danmer89@gmail.com>
2017-01-29 22:37:24 +0300

    100 from 100

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,41 @@
-class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
-    }
-
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
-}
+class GuessingGame 
+		{
+			constructor() 
+			{
+				this.tempNumber = null;
+				this.minIndex = null
+				this.maxIndex = null
+			}
+	
+			setRange(min, max) {
+				this.minIndex = min;
+				this.maxIndex = max;
+				this.array = [];
+				for (var i = min; i <= max ; i++) {
+					this.array.push(i);
+				}
+			}
+	
+			guess() {				
+				this.tempNumber = this.array[Math.floor(this.array.length/2)];				
+				return this.tempNumber;
+			}
+	
+			lower() {
+				this.array = []
+				for (var i = this.minIndex; i <= this.tempNumber; i++) {
+					this.array.push(i);
+				}
+				this.maxIndex = this.array[this.array.length - 1];
+			}
+	
+			greater() {
+				this.array = [];
+				for (var i = this.tempNumber; i <= this.maxIndex; i++) {
+					this.array.push(i);
+				}
+				this.minIndex = this.array[0];
+			}
+		}
 
 module.exports = GuessingGame;
